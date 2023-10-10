5b0c9cd2f6ff1dfd70f2dee4a70ed45d3cad1830
Kjell Zijlemaker <kjell.zijlemaker@live.nl>
2023-10-10 14:50:12 +0200

    Add defaults and default homepage content (#4)
    #major
    
    This release will show the default homepage content. Furthermore, versions have been restricted and badges have been added.

--- kjellzijlemaker/personalwebsite/views.py
+++ kjellzijlemaker/personalwebsite/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(requests):
     return render(requests, "home.html")
+
+
+def resume(requests):
+    return render(requests, "resume.html")
+
+
+def contact(requests):
+    return render(requests, "contact.html")
